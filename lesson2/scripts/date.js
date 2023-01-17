3aa0473d8e410cae9fd0b918583ed8df26fef231
christophercortes <christopherctes@hotmail.com>
2023-01-17 03:33:57 -0300

    small for design principles

---
+++ lesson2/scripts/date.js
@@ -0,0 +1,5 @@
+const options = document.lastModified;
+document.getElementById('lastupdated'). textContent = new Date().toLocaleTimeString('en-US', options);
+
+const year = new Date();
+document.getElementById('date'). textContent = new Date().getFullYear('en-US', year);
