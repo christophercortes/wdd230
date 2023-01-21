717ea92887d28d4ab0ef59c5e5d74a33cca46c43
christophercortes <christopherctes@hotmail.com>
2023-01-21 05:10:49 -0300

    chamber of commerce

---
+++ lesson3/scripts/date.js
@@ -0,0 +1,4 @@
+document.getElementById('lastupdated'). textContent = document.lastModified;
+
+const year = new Date();
+document.getElementById('date'). textContent = new Date().getFullYear('en-US', year);
