538454f4d11fa9cb0db4ebc5793d2fb229a9ef93
christophercortes <christopherctes@hotmail.com>
2023-01-29 03:43:15 -0300

    adding footer

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,6 +1,10 @@
 function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("navigation").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
 }
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
+
+document.getElementById('currentdate'). textContent = document.lastModified;
+const year = new Date();
+document.getElementById('year'). textContent = new Date().getFullYear('en-US', year);
