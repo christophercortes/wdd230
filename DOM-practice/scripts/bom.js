b6ed1329042fb4068a63fd8743f807ec543027a6
christophercortes <christopherctes@hotmail.com>
2023-02-02 20:11:18 -0300

    DOM practice

---
+++ DOM-practice/scripts/bom.js
@@ -0,0 +1,22 @@
+
+const list = document.querySelector('#list')
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    const currValue = input.value;
+    input.value = '';
+    const liElement = document.createElement('li');
+    const listText = document.createElement('span');
+    const deleButton = document.createElement('button');
+    liElement.appendChild(listText);
+    listText.textContent = currValue;
+    liElement.appendChild(deleButton);
+    deleButton.textContent = 'X';
+    list.appendChild(liElement);
+    deleButton.addEventListener('click', () => {
+        list.removeChild(liElement);
+    });
+
+    input.focus();
+});
