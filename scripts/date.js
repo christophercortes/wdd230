9d654410de3885a314948eb798feb28ad9907e37
christophercortes <christopherctes@hotmail.com>
2023-04-22 22:04:10 -0400

    final home page

---
+++ scripts/date.js
@@ -0,0 +1,3 @@
+document.getElementById('currentdate'). textContent = document.lastModified;
+const year = new Date();
+document.getElementById('year'). textContent = new Date().getFullYear('en-US', year);
