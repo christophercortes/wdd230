a6a6747b687c14b4c3bea596c1aa9ef4f0c96c27
christophercortes <christopherctes@hotmail.com>
2023-06-23 15:50:47 -0400

    windchill calculation

--- chamber/scripts/weatherapi.js
+++ chamber/scripts/weatherapi.js
@@ -1,16 +1,16 @@
-// select HTML elements in the document 
-const currentTemp = document.querySelector("#current-temp");
-const weatherIcon = document.querySelector("#weather-icon");
-const captionDesc = document.querySelector("figcaption");
+const currentTemp = document.querySelector(".box3two");
+const weatherIcon = document.querySelector(".emoji");
+const captionDesc = document.querySelector(".weathercondition");
+const currentDegree = document.querySelector(".windspeed");
 
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=8858983065863e23c3d5a33999223d4c";
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=-33.5947&lon=-71.6075&units=metric&appid=8858983065863e23c3d5a33999223d4c";
 
 async function apiFetch() {
     try {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            console.log(data); // this is for testing the call
+            //console.log(data);
             displayResults(data);
         } else {
             throw Error(await response.text());
@@ -22,6 +22,7 @@ async function apiFetch() {
 
 function displayResults(weatherData) {
     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    currentDegree.innerHTML = `${weatherData.wind.speed.toFixed(1)}`;
 
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,6 +1,34 @@
-var temperature = 68;
-var windSpeed = 3.1;
-var windChill = (35.74 + (0.6215 + temperature))-(35.75 * Math.pow(windSpeed,0.16));
+const jsonWeather = "https://api.openweathermap.org/data/2.5/weather?lat=-33.5947&lon=-71.6075&units=metric&appid=8858983065863e23c3d5a33999223d4c";
 
-var windChill = Math.round(windChill);
-document.getElementById("windChill").innerHTML = windChill;
+fetch(jsonWeather)
+    .then(response => response.json())
+    .then(data => {
+        const calculateT = data.main.temp;
+        const calculateS = data.wind.speed;
+
+        const windChill = calculateWindChill(calculateT, calculateS);
+
+        const windChillText = windChill !== null ? windChill.toFixed(1) : "N/A"
+        const windChillElement = document.getElementById("windChill");
+        windChillElement.textContent = windChillText;
+    })
+
+    .catch(error => {
+        console.log("Error fetching JSON:", error);
+    });
+
+function calculateWindChill(calculateT, calculateS) {
+    if (calculateT <= 50 && calculateS >= 3) {
+        const windChillIndex = 35.74 + (0.6215 * calculateT) - (35.75 * Math.pow(calculateS, 0.16)) + (0.4275 * calculateT *Math.pow(calculateS, 0.16));
+        return windChillIndex;
+    } else {
+        return null;
+    }
+}
+
+// var temperature = 68;
+// var windSpeed = 3.1;
+// var windChill = (35.74 + (0.6215 + temperature))-(35.75 * Math.pow(windSpeed,0.16));
+
+// var windChill = Math.round(windChill);
+// document.getElementById("windChill").innerHTML = windChill;
