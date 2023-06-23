a9e09e978d7e8145709fd3231faaae9343430e6d
christophercortes <christopherctes@hotmail.com>
2023-06-23 16:08:05 -0400

    adding weather information from the web credit

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -25,10 +25,3 @@ function calculateWindChill(calculateT, calculateS) {
         return null;
     }
 }
-
-// var temperature = 68;
-// var windSpeed = 3.1;
-// var windChill = (35.74 + (0.6215 + temperature))-(35.75 * Math.pow(windSpeed,0.16));
-
-// var windChill = Math.round(windChill);
-// document.getElementById("windChill").innerHTML = windChill;
